de4f5b82c9cfe4ee6452a5c1ee3a0104d9896ddc
Mr-Himanshu01 <girasehimanshu2023@gmail.com>
2025-11-05 22:08:47 +0530

    Assignment task Files!

---
+++ Assignment_1_Task_1.py
@@ -0,0 +1,22 @@
+First_Number = int(input("Enter the first number:"))
+Second_Number = int(input("Enter the second number:"))
+
+# do the required basic mathematical operations with given input
+
+Addition = First_Number + Second_Number
+
+Subtraction = First_Number - Second_Number
+
+Multiplication = First_Number * Second_Number
+
+Division = First_Number / Second_Number
+
+print("Addition:", Addition)
+
+print("Subtraction:", Subtraction)
+
+print("Multiplication:", Multiplication)
+
+print("Division:", Division)
+
+
