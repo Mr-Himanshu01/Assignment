0dbd7f7bdfabab0383b7d056ed4e7318d9b407d6
Mr-Himanshu01 <girasehimanshu2023@gmail.com>
2025-11-06 11:49:07 +0530

    Update Assignment_1_Task_1.py

--- Assignment_1_Task_1.py
+++ Assignment_1_Task_1.py
@@ -1,3 +1,5 @@
+# Task 1: Basic Mathematical Operations
+
 First_Number = int(input("Enter the first number:"))
 Second_Number = int(input("Enter the second number:"))
 
@@ -18,5 +20,17 @@ print("Subtraction:", Subtraction)
 print("Multiplication:", Multiplication)
 
 print("Division:", Division)
+
+
+# Task 2: Greet User With his Name
+
+First_Name = input("Enter your first name:")
+Last_Name = input("Enter your last name:")
+
+# do the Concatenation with the first name and last name into a full name with given input
+
+Full_Name = First_Name +" " + Last_Name
+
+print("Hello, ", Full_Name, "! ", "Welcome to the python program", sep = "")
 
 
