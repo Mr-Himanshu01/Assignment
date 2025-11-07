fbda6cd04429b6f0b5fded1b7cc69e83e048bd9e
Mr-Himanshu01 <girasehimanshu2023@gmail.com>
2025-11-07 22:08:22 +0530

    Assignment_1_Task_2.py

--- Assignment_1_Task_2.py
+++ Assignment_1_Task_2.py
@@ -1,1 +1,24 @@
 
+# Task 2: Greet User With his Name
+""" This program do Greet to the User like 
+Hello, User Name! Welcome to the python program.
+- Here User have to give inputs of the first Name and last Name. 
+- Example:  Inputs, 
+            Enter your first name: Himanshu
+            Enter your last name: Girase
+            
+            Result,
+            Hello, Himanshu Girase! Welcome to the python program          
+"""
+
+First_Name = input("Enter your first name: ")
+Last_Name = input("Enter your last name: ")
+
+# do the Concatenation with the first name and last name into a full name with given input
+
+Full_Name = First_Name +" " + Last_Name
+
+print("Hello, ", Full_Name, "! ", "Welcome to the python program", sep = "")
+
+
+
